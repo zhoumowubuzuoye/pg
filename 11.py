64fddbe23d58e59c0f21ef173f702dc8615f7765
zhoumowubuzuoye <www.1078637049@qq.com>
2022-10-27 14:46:05 +0800

    cesghi

---
+++ 11.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/27 10:48
+# @Author  : 周末勿补作业
+# @FileName: 11.py
+# @Software: PyCharm
+
+massage1 = 'HELLO'
+massage2 = 'PYTHON'
+print(massage2 + massage1)
---
+++ module/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/27 10:50
+# @Author  : 周末勿补作业
+# @FileName: __init__.py.py
+# @Software: PyCharm
+
+message = "Hello Python Crash Course reader!"
+print(message)
